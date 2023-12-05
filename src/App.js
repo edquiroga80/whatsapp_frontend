8ffad6b1b25984754e84a9774d67f30d3a55532d
Eduardo Quiroga <quirogagis@gmail.com>
2023-12-05 10:49:48 -0300

    Agregamos Tailwindcss a nuestra aplicacion

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { ChatIcon } from "./svg";
 function App() {
   return (
     <div>
-      <h1>Hola desde el FRONTEND</h1>
-      <ChatIcon />
+      <h1 className="text-green_1 font-bold bg-dark_bg_2">Hola desde el FRONTEND</h1>
+      <ChatIcon className="fill-amber-500"/>
     </div>
   );
 }
