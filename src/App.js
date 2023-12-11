cad27eafd50f9145235e55eb91c3d380d8360662
Eduardo Quiroga <quirogagis@gmail.com>
2023-12-11 09:09:17 -0300

    Configuracion FrontEnd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ChatIcon } from "./svg";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
 function App() {
   return (
     <div>
-      <h1 className="text-green_1 font-bold bg-dark_bg_2">Hola desde el FRONTEND</h1>
-      <ChatIcon className="fill-amber-500"/>
+      <h1 className="text-green_1 font-bold bg-dark_bg_2">
+        Hola desde el FRONTEND
+      </h1>
+      <ChatIcon className="fill-amber-500" />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
